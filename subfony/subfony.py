9588b575414639ca14e8dc9254fcfc768ddea179
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-07-25 12:12:31 +0200

    Configuration for Subfony

--- subfony/subfony.py
+++ subfony/subfony.py
@@ -1,28 +1,27 @@
 import sublime
 import sublime_plugin
-import thread
-import subprocess
 import os
-import functools
+
+
+class Pref:
+    @staticmethod
+    def load():
+        settings = sublime.load_settings('subfony.sublime-settings')
+        Pref.php_bin = settings.get('php_bin', '/usr/bin/php')
+        Pref.console_bin = settings.get('console_bin', 'app/console')
+        Pref.src_dir = settings.get('src_dir', 'src')
+
 
 st_version = 2
 if sublime.version() == '' or int(sublime.version()) > 3000:
     st_version = 3
 
+if st_version == 2:
+    Pref.load()
 
-class ShowInPanel:
 
-    def __init__(self, window):
-        self.window = window
-
-    def display_results(self):
-        self.panel = self.window.get_output_panel("exec")
-        self.window.run_command("show_panel", {"panel": "output.exec"})
-        # self.panel.settings().set("color_scheme", THEME)
-        # self.panel.set_syntax_file(SYNTAX)
-        # if HIDE_PANEL:
-            # self.window.run_command("hide_panel")
-            # self.panel.settings().set("color_scheme", THEME)
+def plugin_loaded():
+    Pref.load()
 
 
 class SubfonyGenerateBundleCommand(sublime_plugin.WindowCommand):
@@ -35,21 +34,23 @@ class SubfonyGenerateBundleCommand(sublime_plugin.WindowCommand):
         self.view = self.window.active_view()
 
     def on_done(self, text):
-        cmd = ['/usr/local/opt/php54/bin/php', 'app/console', 'generate:bundle', '--dir=src', '--namespace=' + text, '--no-interaction']
+        if not self.view or not self.view.file_name():
+            sublime.status_message('A file must be open. Sorry.')
+            return
+        cwd = os.path.dirname(self.view.file_name())
+        while not os.path.exists(cwd + '/app') or cwd == '/' or cwd == '':
+            cwd = os.path.dirname(cwd)
 
-        self.run_shell_command(cmd, '/Users/florian/Workspace/playground/subfony')
+        if cwd == '/' or cwd == '':
+            sublime.status_message('You\'re not in a Symfony2 application.')
+            return
+
+        cmd = [Pref.php_bin, Pref.console_bin, 'generate:bundle', '--dir=' + Pref.src_dir, '--namespace=' + text, '--no-interaction']
+        self.run_shell_command(cmd, cwd)
 
     def run_shell_command(self, command, working_dir):
         if not command:
             return False
-        sublime.status_message(' '.join(command))
-        # if BEFORE_CALLBACK:
-        #     os.system(BEFORE_CALLBACK)
-        # if AFTER_CALLBACK:
-        #     command += " ; " + AFTER_CALLBACK
-        # self.save_test_run(command, working_dir)
-        # if COMMAND_PREFIX:
-        #     command = COMMAND_PREFIX + ' ' + command
         self.view.window().run_command("exec", {
             "cmd": command,
             "shell": False,
@@ -65,3 +66,13 @@ class SubfonyGenerateBundleCommand(sublime_plugin.WindowCommand):
 
     def window(self):
         return self.view.window()
+
+
+class ShowInPanel:
+
+    def __init__(self, window):
+        self.window = window
+
+    def display_results(self):
+        self.panel = self.window.get_output_panel("exec")
+        self.window.run_command("show_panel", {"panel": "output.exec"})
